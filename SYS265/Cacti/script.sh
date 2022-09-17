ba4b189f41bce4d34053901e663d6bed4df5a7e3
HusseinM5 <90108325+HusseinM5@users.noreply.github.com>
2022-09-17 15:39:18 -0400

    Update script.sh

--- SYS265/Cacti/script.sh
+++ SYS265/Cacti/script.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+echo $'\e[1;32m'"sudo ./script.sh <username> <password>"$'\e[0m'
 if [ "$EUID" -ne 0 ]
   then
-        echo "Please run as root"
+        echo $'\e[1;31m'"Please run as root"$'\e[0m'
         exit
 fi
 
+echo $'\e[1;32m'"Configuring SensuGo"$'\e[0m'
+echo ""
 cd /tmp
 curl -s https://packagecloud.io/install/repositories/sensu/stable/script.rpm.sh | bash
 yum -y install sensu-go-backend
 curl -L https://docs.sensu.io/sensu-go/latest/files/backend.yml -o /etc/sensu/backend.yml
 service sensu-backend start
 service sensu-backend status
+
+echo $'\e[1;32m'"Sensu-Backend is running?"$'\e[0m'
+echo ""
 export SENSU_BACKEND_CLUSTER_ADMIN_USERNAME=$1
 export SENSU_BACKEND_CLUSTER_ADMIN_PASSWORD=$2
 sensu-backend init
+echo ""
 curl http://127.0.0.1:8080/health
-
-echo "Login to http://localhost:3000/"
+echo ""
+echo $'\e[1;32m'"Healthy is set to True?"$'\e[0m'
+echo ""
+echo $'\e[1;32m'"Login to the link below using the credentials you set"$'\e[0m'
+echo $'\e[1;32m'"http://localhost:3000/"$'\e[0m'
