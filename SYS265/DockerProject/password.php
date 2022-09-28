73652fefd3ceb01e22230957d4d90a7cd672dc39
HusseinM5 <90108325+HusseinM5@users.noreply.github.com>
2022-09-28 10:26:33 -0400

    Update password.php

--- SYS265/DockerProject/password.php
+++ SYS265/DockerProject/password.php
@@ -7,18 +7,18 @@
 </head>
 <body>
   <div class="header">
-  	<h2>Register</h2>
+        <h2>Register</h2>
   </div>
-	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_password">Register</button>
-  	</div>
+
+  <form method="post" action="password.php">
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+          <label>Password</label>
+          <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+          <button type="submit" class="btn" name="reg_password">Register</button>
+        </div>
   </form>
 </body>
 </html>
